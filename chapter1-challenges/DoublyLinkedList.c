7cb55df5127e9e93ab99248a116bd6315d873cbf
Dennis Forster <66913269+Prometheus627@users.noreply.github.com>
2023-07-23 16:22:00 +0200

    Update DoublyLinkedList.c

--- chapter1-challenges/DoublyLinkedList.c
+++ chapter1-challenges/DoublyLinkedList.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "string.h"
+#include <stdbool.h>
 
 struct Node {
     char data[30];
@@ -32,9 +33,55 @@ int find(Node node) {
         ++index;
         iterator = iterator->next;
     }
+    return -1;
+}
+
+bool delete(Node node) {
+    Node *iterator = head;
+
+    while(iterator != NULL) {
+        if(strcmp(node.data, iterator->data) == 0) {
+            iterator->predecessor->next = iterator->next;
+            return true;
+        }
+        iterator = iterator->next;
+    }
+    return false;
+}
+
+bool insert(Node node, int index) {
+    Node *iterator = head;
+    Node *temp;
+
+    if(index == 0) {
+        if(head == NULL) {
+            head = &node;
+        } else {
+            head->predecessor = &node;
+            head = head->predecessor;
+        }
+        return true;
+    }
+
+    for(; index > 0; index--) {
+        if(iterator->next != NULL) {
+            iterator = iterator->next;
+        } else {
+            return false;
+        }
+    }
+    temp = iterator->next;
+    iterator->next = &node;
+    if(temp != NULL) {
+        node.next = temp->next;
+    }
+    return true;
 }
 
 int main() {
-    printf("Hello, World!\n");
+    Node n1;
+    strcpy(n1.data, "test");
+    insert(n1, 0);
+    printList();
     return 0;
 }
